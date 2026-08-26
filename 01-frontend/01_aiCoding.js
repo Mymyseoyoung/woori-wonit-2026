cb30e95cf118b6796eda209eae19e36f3cac72a2
박서영 <emilypark0721@gmail.com>
2026-08-26 16:58:24 +0900

    feat: ai에이전트 활용하는 방법 추가

---
+++ 01-frontend/01_aiCoding.js
@@ -0,0 +1,13 @@
+// 두 수를 더하는 함수를 작성해줘. ( 이미 만들어진 코드에는 새로운 프롬프팅이 적용 안됨 ! )
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// 두 수를 빼는 함수 
+//tab으로 선택, esc로 자동완성 취소
+//변경할 영역을 드래그 해서 선택한 후 , Ctrl + I 로 질문창을 열어서 수정 ! 
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// Ctrl + Shift + Alt + L 로 질문창을 엽니다 . 
